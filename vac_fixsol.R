93301623f95fbb8936f5918cd6376c639607c7d9
krizovakk <krizovakk@gmail.com>
2020-10-02 10:50:01 +0200

    VUCHS script modified

--- vac_fixsol.R
+++ vac_fixsol.R
@@ -1,3 +1,12 @@
+# 201902
+# Pro Vaclava
+# Vyzkum v chovu skotu
+# -analyza a grafy
+# sezona = "year"
+# hloubky = d4 atd
+# CESKY !!!
+# grafy: w8 h4 dpi500 base size=15
+
 # base --------------------------------------------------------------------
 
 # install.packages("tidyverse")
@@ -10,48 +19,54 @@ require(reshape2)
 
 # PENRES ------------------------------------------------------------------
 
-pen <- read_excel("data/penres.xlsx")
+pen <- read_excel("data/vuchs20.xlsx", sheet = 1)
 
-pen$var[pen$var == 7] <- "ZF"
-pen$var[pen$var == 8] <- "ZF_SOL"
-pen$var[pen$var == 9] <- "C"
-pen$var[pen$var == 10] <- "SOL"
+# pen$var <- as.factor(pen$var, levels = "1011", "1006", "1007", "1001", "1002")
+
+pen$var[pen$var == 1001] <- "hnSOL"
+pen$var[pen$var == 1002] <- "hnFIXSOL"
+pen$var[pen$var == 1006] <- "hn"
+pen$var[pen$var == 1007] <- "hnFIX"
+pen$var[pen$var == 1011] <- "NPK"
+
+pen$var <- factor(pen$var, levels = c("NPK", "hn", "hnFIX", "hnSOL", "hnFIXSOL"))
 
 penl <- pen %>% 
-  melt(id.vars = c("seas", "var"), variable.name = ("depth"), value.name = "penres")
+  melt(id.vars = c("year", "var"), variable.name = ("depth"), value.name = "penres")
 
-p15 <- pen %>% 
-  filter(seas == 2015)
+p18 <- pen %>% 
+  filter(year == 2018)
 
-p17 <- pen %>% 
-  filter(seas == 2017)
+p19 <- pen %>% 
+  filter(year == 2019)
 
-p15l <- p15 %>% 
-  select(var, cm4, cm8, cm12, cm16, cm20) %>% 
+p18l <- p18 %>% 
+  select(var, d4, d8, d12, d16, d20) %>% 
   melt(id.vars = c("var"), variable.name = ("depth"), value.name = "penres")
 
-p17l <- p17 %>% 
-  select(var, cm4, cm8, cm12, cm16, cm20) %>% 
+p19l <- p19 %>% 
+  select(var, d4, d8, d12, d16, d20) %>% 
   melt(id.vars = c("var"), variable.name = ("depth"), value.name = "penres")
 
 # penres explorative ------------------------------------------------------
 
 dept <- c("20", "16", "12", "8", "4")
 
-# both years in one plot
-penl$depth <- factor(penl$depth, levels = c("cm20", "cm16", "cm12", "cm8", "cm4"), labels = dept)
+#  VUCHS both years in one plot
+penl$depth <- factor(penl$depth, levels = c("d20", "d16", "d12", "d8", "d4"), labels = dept)
 
-penl$seas <- factor(penl$seas, levels = c("2017", "2015"))
+penl$year <- factor(penl$year, levels = c("2019", "2018"))
 
-ggplot(penl, aes(depth, penres, fill=seas))+
+ggplot(penl, aes(depth, penres, fill=year))+
   geom_bar(aes(width = 0.5), stat = "summary", fun.y = "mean", 
            position = position_dodge())+
-  scale_fill_manual(values=c("grey30", "darkgrey"), breaks = rev(levels(penl$seas)))+
+  scale_fill_manual(values=c("grey30", "darkgrey"), breaks = rev(levels(penl$year)))+
   coord_flip()+
   facet_grid(. ~ var)+
-  labs(y = "\nPenetration Resistance [MPa]", x = "Depth [cm]", fill = "", title = "")+
-  theme_minimal()
-ggsave("penres_both.png", device = "png", width = 8, height = 4, dpi = 500)
+  labs(y = "\npenetrační odpor [MPa]", x = "hloubka [cm]", fill = "", title = "")+
+  theme_classic(base_size = 15)+
+  theme(legend.position="top")
+ggsave("penres_both.png", path = "plots", device = "png", width = 8, height = 4, dpi = 500)
 
 # separate plots for 2015 nad 2017
 p15l$depth <- factor(p15l$depth, levels = c("cm20", "cm16", "cm12", "cm8", "cm4"), labels = dept)
@@ -76,20 +91,15 @@ ggsave("penres17.png", device = "png", width = 6, height = 3, dpi = 500)
 
 # penres analysis ---------------------------------------------------------
 
-hist(pen$cm4)
-hist(pen$cm8)
-hist(pen$cm12)
-hist(pen$cm16)
-hist(pen$cm20)
+# 2018
 
-#2015
-pairwise.wilcox.test(p15$cm4, p15$var,
+pairwise.wilcox.test(p18$d4, p18$var,
                      p.adjust.method = "BH")
 
 # significant difference is where p-value < 0.05
 # diff everywhere except Var 4 & control
 
-pairwise.wilcox.test(p15$cm8, p15$var,
+pairwise.wilcox.test(p18$d8, p18$var,
                      p.adjust.method = "BH")
 
 pairwise.wilcox.test(p15$cm12, p15$var,
@@ -118,7 +128,7 @@ pairwise.wilcox.test(p15$cm20, p15$var,
   pairwise.wilcox.test(p17$cm20, p17$var,
                        p.adjust.method = "BH")
 
-# UNITD -------------------------------------------------------------------
+# VUCHS NEPOCITA UNITD -------------------------------------------------------------------
 
 uni <- read_excel("data/unitd.xlsx")
   
@@ -142,7 +152,7 @@ u15l <- u15 %>%
 u17l <- u17 %>% 
   select(var, unitd)
 
-# unitd explorative -------------------------------------------------------
+# VUCHS NEPOCITA unitd explorative -------------------------------------------------------
 
 # both years
 
@@ -218,7 +228,7 @@ ggsave("unitd17_percentage.png", device = "png", width = 6, height = 3, dpi = 50
 #   theme_minimal()
 # ggsave("unitd17.png", device = "png", width = 6, height = 3, dpi = 500)
 
-# unitd analysis ----------------------------------------------------------
+# VUCHS NEPOCITA unitd analysis ----------------------------------------------------------
 
 pairwise.wilcox.test(u15$unitd, u15$var,
                      p.adjust.method = "BH")
@@ -292,22 +302,31 @@ pairwise.wilcox.test(i17$inf, i17$var,
                      p.adjust.method = "BH")
 
 
+
 # RBD ---------------------------------------------------------------------
 
-rbd <- read_excel("data/rbd.xlsx")
+rbd <- read_excel("data/vuchs20.xlsx", sheet = 2)
 
-rbd <- rbd %>% 
-  melt(id.vars = c("var"), variable.name = ("year"), value.name = "rbd")
+rbd$var[rbd$var == 1001] <- "hnSOL"
+rbd$var[rbd$var == 1002] <- "hnFIXSOL"
+rbd$var[rbd$var == 1006] <- "hn"
+rbd$var[rbd$var == 1007] <- "hnFIX"
+rbd$var[rbd$var == 1011] <- "NPK"
+
+rbd$var <- factor(rbd$var, levels = c("NPK", "hn", "hnFIX", "hnSOL", "hnFIXSOL"))
+
+# rbd <- rbd %>% 
+#   melt(id.vars = c("var"), variable.name = ("year"), value.name = "rbd")
 
 rbd$year <- as.factor(rbd$year)
 
-ggplot(rbd, aes(var, rbd, fill = year))+
-  geom_bar(stat="identity", position=position_dodge())+
+ggplot(rbd, aes(var, roh, fill = year))+
+  geom_bar(aes(width = 0.5), stat="identity", position=position_dodge())+
   scale_fill_manual(values=c("darkgrey","grey30"))+
-  labs(y = expression("Reduced Bulk Density [ g"~ cm^-3~"]"), x = "", fill = "")+
+  labs(y = expression("objemová hmotnost [ g."~cm^-3~"]"), x = "", fill = "")+
   theme(legend.title=element_blank())+
-  theme_minimal()
-ggsave("rbd_bothyears.png", device = "png", width = 6, height = 3, dpi = 500)
+  theme_classic(base_size = 15)
+ggsave("rbd_bothyears.png", path = "plots", device = "png", width = 8, height = 4, dpi = 500)
 
 # hraju si s grafem, aby byl vyraznejsi
 
@@ -324,8 +343,7 @@ ggsave("rbd_bothyears_ylim.png", device = "png", width = 6, height = 3, dpi = 50
   geom_bar(aes(width = 0.5),stat = "summary", fun.y = "mean",
            position = position_dodge())
   
-
-# METEO -------------------------------------------------------------------
+# VUCHS NEPOCITA METEO -------------------------------------------------------------------
 
 met <- read_excel("data/meteo.xlsx")
 colnames(met) [1] <- "month"
